4eb16e822a76e19605e4beeda56af36a4c320e76
DrEden33773 <edwardwang33773@gmail.com>
2023-11-02 10:18:01 +0800

    prepared to add error_sync  feature

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,6 +1,4 @@
-// #![allow(dead_code)]
-
-mod methods;
+pub mod methods;
 
 use crate::{
   ast::ProgramExpr,
@@ -14,77 +12,67 @@ pub struct Parser<'a> {
   lexer: Lexer<'a>,
   ast_entry: Option<Box<ProgramExpr>>,
   has_error: bool,
+  panic_mode: bool,
 }
 
 impl<'a> Parser<'a> {
   fn consume_next(&mut self, token: Token) {
-    if let Some(t) = self.lexer.peek() {
-      if let Token::LexicalError(err) = t.to_owned() {
-        eprintln!("{}", err);
-        self.has_error = true;
-      } else if *t != token {
-        let unexpected_t = t.to_owned();
-        self.lexer.panic_compile_error(
-          CompileError::syntax_error_template(),
-          format!("Expected `{}`, but got `{}`", token, unexpected_t),
-        );
-      } else {
-        self.lexer.next();
-      }
-    } else {
+    let t = self.lexer.peek().unwrap();
+
+    if let Token::LexicalError(err) = t.to_owned() {
+      eprintln!("{}", err);
       self.has_error = true;
+    } else if *t != token {
+      let unexpected_t = t.to_owned();
+      self.lexer.panic_compile_error(
+        CompileError::syntax_error_template(),
+        format!("Expected `{}`, but got `{}`", token, unexpected_t),
+      );
+    } else {
+      self.lexer.next();
     }
   }
 
   fn match_next(&mut self, token: Token) -> bool {
-    if let Some(t) = self.lexer.peek() {
-      if let Token::LexicalError(err) = t.to_owned() {
-        eprintln!("{}", err);
-        self.has_error = true;
-        false
-      } else {
-        *t == token
-      }
-    } else {
+    let t = self.lexer.peek().unwrap();
+
+    if let Token::LexicalError(err) = t.to_owned() {
+      eprintln!("{}", err);
       self.has_error = true;
       false
+    } else {
+      *t == token
     }
   }
 
   fn consume_next_identifier(&mut self) -> Result<String, ()> {
-    if let Some(t) = self.lexer.peek() {
-      if let Token::LexicalError(err) = t {
-        eprintln!("{}", err);
-        self.has_error = true;
-        Err(())
-      } else if let Token::Identifier(id) = t {
-        let id = id.to_owned();
-        self.lexer.next();
-        Ok(id.to_owned())
-      } else {
-        Err(())
-      }
-    } else {
+    let t = self.lexer.peek().unwrap();
+
+    if let Token::LexicalError(err) = t {
+      eprintln!("{}", err);
       self.has_error = true;
+      Err(())
+    } else if let Token::Identifier(id) = t {
+      let id = id.to_owned();
+      self.lexer.next();
+      Ok(id.to_owned())
+    } else {
       Err(())
     }
   }
 
   fn consume_next_integer(&mut self) -> Result<i64, ()> {
-    if let Some(t) = self.lexer.peek() {
-      if let Token::LexicalError(err) = t {
-        eprintln!("{}", err);
-        self.has_error = true;
-        Err(())
-      } else if let Token::Integer(num) = t {
-        let num = num.to_owned();
-        self.lexer.next();
-        Ok(num)
-      } else {
-        Err(())
-      }
-    } else {
+    let t = self.lexer.peek().unwrap();
+
+    if let Token::LexicalError(err) = t {
+      eprintln!("{}", err);
       self.has_error = true;
+      Err(())
+    } else if let Token::Integer(num) = t {
+      let num = num.to_owned();
+      self.lexer.next();
+      Ok(num)
+    } else {
       Err(())
     }
   }
@@ -96,15 +84,16 @@ impl<'a> Parser<'a> {
       lexer: Lexer::new(ctx),
       ast_entry: None,
       has_error: false,
+      panic_mode: false,
     }
   }
 
   pub fn parse(&mut self) -> &mut Self {
     let program_expr = self.parse_program();
-    self.ast_entry = Some(program_expr.into());
     if self.has_error {
       panic!("Errors above occurred (during `parsing`), compiling stopped ...")
     }
+    self.ast_entry = Some(program_expr.into());
     self
   }
 
