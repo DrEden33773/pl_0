bf9af45790571b74cc9d879a627159a974458a94
DrEden33773 <edwardwang33773@gmail.com>
2023-11-03 19:31:38 +0800

    better demo

--- examples/parser/multi_err.pas
+++ examples/parser/multi_err.pas
@@ -1,13 +1,13 @@
 program ;
-var a, b, c, 中;
+var a, b, c;
 begin
   a    1;
   b :=  ;
-  中 := 3;
+  中 : 3;
   if 1 = 1 then
     write(1
   else
     write 0);
-  c : 1;
-  write a + b + c + 中
+  write a + b + c;
+  wr写te(1)
 end
