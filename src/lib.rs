21a9ad5477f3fd4b919ecf58ba0e7c245465ed75
DrEden33773 <edwardwang33773@gmail.com>
2023-11-29 14:55:05 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 use once_cell::sync::Lazy;
 
+pub mod bytecode;
 pub mod error;
 pub mod lexer;
 pub mod parser;
 pub mod pest_parser;
 pub mod sat;
 pub mod util;
+pub mod vm;
 
 pub static SEP: Lazy<String> = Lazy::new(|| "=".repeat(60));
