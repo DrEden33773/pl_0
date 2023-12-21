5b308cca2abf6cc807854a7daab3f38373320a78
DrEden33773 <edwardwang33773@gmail.com>
2023-12-21 19:07:42 +0800

    better format

--- src/lib.rs
+++ src/lib.rs
@@ -17,5 +17,5 @@ pub mod util;
 pub mod value;
 pub mod vm;
 
-pub static SEP: Lazy<String> = Lazy::new(|| "=".repeat(60));
-pub static LINE: Lazy<String> = Lazy::new(|| "-".repeat(60));
+pub static SEP: Lazy<String> = Lazy::new(|| "=".repeat(70));
+pub static LINE: Lazy<String> = Lazy::new(|| "-".repeat(70));
